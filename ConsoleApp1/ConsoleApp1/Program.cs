fe474477b677295a3c476104e1556113cd4ea234
Dhary01 <Dharym@hotmail.com>
2022-03-16 12:20:48 +0300

    I made to characters and compared their age by if statment

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace ConsoleApp1
+
+{
+    class Program
+
+
+    { static void Main(string[] args)
+
+
+        { string Charactername = "subzero";
+
+            int CharacterHeight = 2;
+
+            string CharacterName = "Mario";
+
+            int Characterspeed = 3;
+
+            int subzeroage = 14;
+
+            string superpower1 = "Fly";
+
+            string superpower2 = "invisibility";
+
+            string Charactername2 = "Mario";
+
+            int Characterspeed2 = 3;
+
+            int Marioage = 16;
+
+            string superpower3 = "Fly";
+
+            string superpower4 = "invisibility";
+            if (subzeroage > Marioage)
+            {
+                Console.WriteLine("subzero is older");
+            }
+            else if (Marioage > subzeroage)
+            {
+                Console.WriteLine("Mario is older");
+            }
+            else
+            {
+                Console.WriteLine("Neither is older");
+            }
+
+        }
+    }
+}
